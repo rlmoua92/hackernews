562058c5f60d2eb8e7895742a4c0c99dce124856
Ryan Moua <ryanmoua@MacBook-Pro.local>
2018-09-18 22:11:31 -0700

    Add Dismiss button and functionality

--- src/App.js
+++ src/App.js
@@ -25,9 +25,18 @@ class App extends Component {
 
   constructor(props) {
     super(props);
+    
     this.state = {
       list,
     };
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
   }
 
   render() {
@@ -41,6 +50,14 @@ class App extends Component {
               <span>{item.author}</span>
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
+              <span>
+                <button
+                  onClick={() => this.onDismiss(item.objectID)}
+                  type="button"
+                >
+                  Dismiss
+                </button>
+              </span>
             </div>
           )}
       </div>
