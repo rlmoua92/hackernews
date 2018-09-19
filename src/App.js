8ccbddfcbdc8019c2a1cdd7002b190350366ee3e
Ryan Moua <ryanmoua@MacBook-Pro.local>
2018-09-19 00:35:31 -0700

    Add search bar and functionality

--- src/App.js
+++ src/App.js
@@ -21,6 +21,8 @@ const list = [
   },
 ];
 
+const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
 
   constructor(props) {
@@ -28,9 +30,11 @@ class App extends Component {
     
     this.state = {
       list,
+      searchTerm: '',
     };
 
     this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
 
   onDismiss(id) {
@@ -39,10 +43,20 @@ class App extends Component {
     this.setState({ list: updatedList });
   }
 
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
   render() {
     return (
       <div className="App">
-        {this.state.list.map(item => 
+        <form>
+          <input 
+            type="text" 
+            onChange={this.onSearchChange}
+          />
+        </form>
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item => 
             <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
