4d6951929f604a7df915ee5f3b74f4e4ef88897f
Ryan Moua <ryanmoua@MacBook-Pro.local>
2018-09-20 17:14:48 -0700

    Replace static data with API connection

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
 const list = [
   {
     title: 'React',
@@ -29,12 +35,26 @@ class App extends Component {
     super(props);
     
     this.state = {
-      list,
-      searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
 
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    this.setState({ result });
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
   }
 
   onDismiss(id) {
@@ -48,7 +68,10 @@ class App extends Component {
   }
 
   render() {
-    const { list, searchTerm } = this.state
+    const { result, searchTerm } = this.state
+
+    if (!result) { return null; }
+
     return (
       <div className="page">
         <div className="interactions">
@@ -60,7 +83,7 @@ class App extends Component {
           </Search>
         </div>
         <Table 
-          list={list}
+          list={result.hits}
           pattern={searchTerm}
           onDismiss={this.onDismiss}
         />
