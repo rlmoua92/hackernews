33bc8b9b9a95d5915b41c6484433823d534a3384
Ryan Moua <ryanmoua@MacBook-Pro.local>
2018-09-21 14:57:31 -0700

    Add conditional rendering to Table

--- src/App.js
+++ src/App.js
@@ -71,9 +71,6 @@ class App extends Component {
 
   render() {
     const { result, searchTerm } = this.state
-
-    if (!result) { return null; }
-
     return (
       <div className="page">
         <div className="interactions">
@@ -84,11 +81,13 @@ class App extends Component {
             Search
           </Search>
         </div>
-        <Table 
-          list={result.hits}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
+        { result &&
+          <Table 
+            list={result.hits}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          />
+        }
       </div>
     );
   }
